d4d07562e36ab9fe685d426c7f69ff14e65cc96a
Benjandre <benjamin.vraspillai@protonmail.com>
2025-07-16 20:00:49 +0200

    Made some changes.

--- blueprints/home.py
+++ blueprints/home.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 home_bp = Blueprint('home', __name__)
 
@@ -10,3 +10,14 @@ def home_redirect():
 @home_bp.route("/home")
 def home():
     return render_template("Home.html")
+
+def handle_data():
+    send_data()
+    retrieve_data()
+
+def retrieve_data():
+    dwdw
+
+def send_data():
+    area = request.form['']
+    MET_Weather_API = "https://api.met.no/weatherapi/dummy/0.3/precipitation?area=", area, ""
